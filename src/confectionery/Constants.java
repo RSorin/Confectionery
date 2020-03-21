c6f9246fb2c982aae4f0f44ff2630bef463a6007
Radu Sorin <info_eu@yahoo.ro>
2020-03-21 13:02:06 +0200

    Modify number of managers

--- src/confectionery/Constants.java
+++ src/confectionery/Constants.java
@@ -7,7 +7,7 @@ package confectionery;
 
 public class Constants {
 	
-	static int MANAGERS_NUMBER = 4;
+	static int MANAGERS_NUMBER = 6;
 	static int DELIVERY_NUMBER = 5;
 	static int CONFECTIONERS_NUMBER = 15;
 	static int DOUGH_CONFECTIONERS_NUMBER = 5;
